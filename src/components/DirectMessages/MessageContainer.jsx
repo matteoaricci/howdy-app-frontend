cbc084f84bf9483c2b53fd121ad220391055368c
matteoaricci <matteo.a.ricci@gmail.com>
2020-11-23 15:44:52 -0500

    created dm container

---
+++ src/components/DirectMessages/MessageContainer.jsx
@@ -0,0 +1,16 @@
+import React,{useState, useEffect} from 'react';
+
+const MessageContainer = () => {
+
+    useEffect(() => {
+        
+    }, []);
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MessageContainer;
